786561e3403edeb3333d8a800627bf37e257d47c
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-20 17:46:14 +0600

    event listener

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -12,13 +13,23 @@ function App() {
 }
 
 function MyComponent(props){
+
+  const [points,setPoints]= useState(1);
+
   const myStyle ={
-    backgroundColor:'blue'
+    backgroundColor:'#61dafb'
+  }
+
+  const handleAddPoints =() =>{
+    const newPoints= points * 2;
+    setPoints(newPoints);
   }
   return(
     <div style={myStyle}>
       <h1>Brand:{props.brand}</h1>
       <h4>Price:{props.price}</h4>
+      <h3>I have points:{points}</h3>
+      <button onClick={handleAddPoints}>Add Points</button>
     </div>
   )
 }
